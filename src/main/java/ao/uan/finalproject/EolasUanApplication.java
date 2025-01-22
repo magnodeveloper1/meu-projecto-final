5e766b2eca557bece6ba6191adcb19a900db549b
Cristiano Mbongo <cristianombongo@refera.com.br>
2025-01-22 21:50:44 +0100

    initial project

---
+++ src/main/java/ao/uan/finalproject/EolasUanApplication.java
@@ -0,0 +1,13 @@
+package ao.uan.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EolasUanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EolasUanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ao/uan/finalproject/EolasUanApplicationTests.java
@@ -0,0 +1,13 @@
+package ao.uan.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EolasUanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
