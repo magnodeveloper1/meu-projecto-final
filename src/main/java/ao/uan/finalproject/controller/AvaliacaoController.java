fddb547e3e0046e49ea8d085ea885e57e7ba046a
Cristiano Mbongo <cristianombongo19@outlook.com>
2025-03-09 13:35:04 +0100

    .

--- src/main/java/ao/uan/finalproject/controller/AvaliacaoController.java
+++ src/main/java/ao/uan/finalproject/controller/AvaliacaoController.java
@@ -1,6 +1,15 @@
 package ao.uan.finalproject.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/avaliacao")
 public class AvaliacaoController {
     
+    @GetMapping("/")
+    public String screenAvaliacao() {
+        return "";
+    }
 }
