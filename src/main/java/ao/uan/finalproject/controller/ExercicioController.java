ef66086b8bff21575be35a725dae0de70787d23a
Cristiano Mbongo <cristianombongo19@outlook.com>
2025-03-10 17:45:36 +0100

    .

--- src/main/java/ao/uan/finalproject/controller/ExercicioController.java
+++ src/main/java/ao/uan/finalproject/controller/ExercicioController.java
@@ -3,6 +3,7 @@ package ao.uan.finalproject.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ExercicioController {
@@ -14,6 +15,19 @@ public class ExercicioController {
 
     @GetMapping("exercicio/criar")
     public String screenCriar() {
+        return "exercicio/criar.html";
+    }
+
+    @GetMapping("exercicio/criar/2")
+    public String screenCriar2(
+        @RequestParam(name = "source") String encriptInfo
+    ) {
+
+        Object[] values = new Object[8];
+
+        try {
+        } catch(Exception ex) {}
+
         return "exercicio/criar.html";
     }
 }
